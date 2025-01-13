374a26030b154efad42804bb38dacf434d93173f
JaneChun <janechun22@gmail.com>
2025-01-13 16:13:03 +0900

    Placeform - Title 인풋 추가

--- components/places/PlaceForm.js
+++ components/places/PlaceForm.js
@@ -1,15 +1,45 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { View, Text, StyleSheet, ScrollView, TextInput } from 'react-native';
+import { Colors } from '@/constants/colors';
 
 const PlaceForm = () => {
+	const [title, setTitle] = useState('');
+	const titleChangeHandler = (enteredText) => {
+		setTitle(enteredText);
+	};
 	return (
-		<View style={styles.container}>
-			<Text>PlaceForm</Text>
-		</View>
+		<ScrollView style={styles.container}>
+			<View>
+				<Text style={styles.label}>Title</Text>
+				<TextInput
+					style={styles.input}
+					value={title}
+					onChangeText={titleChangeHandler}
+				/>
+			</View>
+		</ScrollView>
 	);
 };
 
 const styles = StyleSheet.create({
-	container: {},
+	container: {
+		flex: 1,
+		padding: 24,
+	},
+	label: {
+		fontWeight: 'bold',
+		marginBottom: 4,
+		color: Colors.primary500,
+	},
+	input: {
+		marginVertical: 8,
+		paddingHorizontal: 4,
+		paddingVertical: 8,
+		fontSize: 16,
+		color: Colors.primary700,
+		borderBottomWidth: 2,
+		backgroundColor: Colors.primary100,
+	},
 });
 
 export default PlaceForm;
